536e7d88bbe782e2eea496d3befd98e9bbfa0194
danila12 <danila@terrabit.ro>
2025-07-24 14:31:34 +0300

    Updated product domain to use `is_storable` in `service_consumable` model.

--- deltatech_service_consumable/__manifest__.py
+++ deltatech_service_consumable/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Consumable",
     "summary": "Service Consumable",
-    "version": "18.0.1.1.3",
+    "version": "18.0.1.1.4",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
