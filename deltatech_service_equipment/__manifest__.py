20e256853e7270f83a8e5347c33291864b39f4dd
Dorin Hongu <dhongu@gmail.com>
2026-01-13 19:23:28 +0200

    Actualizează versiunea modulului în manifest
    Versiunea modulului `deltatech_service_equipment` a fost incrementată de la `18.0.1.1.11` la `19.0.1.1.11` pentru a reflecta modificările și îmbunătățirile recente.

--- deltatech_service_equipment/__manifest__.py
+++ deltatech_service_equipment/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Equipment",
     "summary": "Service Equipment Management",
-    "version": "18.0.1.1.11",
+    "version": "19.0.1.1.11",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
