dcfa5509a66f87fa7a5458aca9ab2bf916b5fd08
Dorin Hongu <dhongu@gmail.com>
2026-01-17 16:15:16 +0200

    Adaugă import pentru `service_equipment_part` și actualizează versiunea
    Importul pentru modelul `service_equipment_part` a fost repoziționat în fișierul `__init__.py` pentru o structurare mai clară. Versiunea modulului din manifest a fost incrementată la `19.0.1.2.0` pentru a reflecta această modificare.

--- deltatech_service_equipment_base/__manifest__.py
+++ deltatech_service_equipment_base/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Equipment Base",
     "summary": "Service Equipment Management",
-    "version": "19.0.1.1.9",
+    "version": "19.0.1.2.0",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
