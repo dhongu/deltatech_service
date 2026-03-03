8ddb8d90a4eaa9df769db4a7c80dad9ccba26939
VoicuStefan2001 <stefanvoicu2009@gmail.com>
2026-03-03 10:36:10 +0200

    adaugare istoric pentru parti

--- deltatech_service_task/models/service_equipment.py
+++ deltatech_service_task/models/service_equipment.py
@@ -8,12 +8,16 @@ class ServiceEquipment(models.Model):
     _inherit = "service.equipment"
 
     task_count = fields.Integer(compute="_compute_task_history")
+    part_history_count = fields.Integer(compute="_compute_task_history")
     check_history_count = fields.Integer(compute="_compute_task_history")
     measurement_history_count = fields.Integer(compute="_compute_task_history")
 
     def _compute_task_history(self):
         for equipment in self:
             equipment.task_count = self.env["project.task"].search_count([("service_equipment_id", "=", equipment.id)])
+            equipment.part_history_count = self.env["project.task.part"].search_count(
+                [("equipment_id", "=", equipment.id)]
+            )
             equipment.check_history_count = self.env["project.task.check"].search_count(
                 [("equipment_id", "=", equipment.id)]
             )
@@ -27,6 +31,17 @@ class ServiceEquipment(models.Model):
         action["domain"] = [("service_equipment_id", "=", self.id)]
         action["context"] = {"default_service_equipment_id": self.id}
         return action
+
+    def action_view_part_history(self):
+        self.ensure_one()
+        return {
+            "name": self.env._("Part History"),
+            "type": "ir.actions.act_window",
+            "res_model": "project.task.part",
+            "view_mode": "list,form",
+            "domain": [("equipment_id", "=", self.id)],
+            "context": {"default_equipment_id": self.id},
+        }
 
     def action_view_check_history(self):
         self.ensure_one()
