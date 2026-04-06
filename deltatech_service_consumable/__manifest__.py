a9c0167df5d76486593d4826d7a27dfbf2b3aeb5
Dorin Hongu <dhongu@gmail.com>
2026-04-06 06:33:01 +0300

    [REM] deltatech_service_consumable: eliminare vizualizări și rapoarte
    Au fost eliminate vizualizările, filtrele și meniurile pentru raportul de eficiență a serviciilor din modulul `deltatech_service_consumable`, împreună cu intrările din fișierul XML asociat. În plus, versiunea modulului a fost incrementată pentru a reflecta această modificare majoră.

--- deltatech_service_consumable/__manifest__.py
+++ deltatech_service_consumable/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Services Consumable",
     "summary": "Service Consumable",
-    "version": "18.0.1.1.6",
+    "version": "19.0.1.1.6",
     "author": "Terrabit, Dorin Hongu",
     "website": "https://www.terrabit.ro",
     "category": "Services/Equipment",
