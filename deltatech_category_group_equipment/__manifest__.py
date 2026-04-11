1650e13763bdf4cc5bc6185bd93771a39222517c
Dorin Hongu <dhongu@gmail.com>
2026-04-11 12:02:45 +0300

    Actualizează versiunea modului deltatech_category_group_equipment
    Versiunea modului a fost crescută de la 18.0.0.0.2 la 19.0.0.0.2 în fișierul __manifest__.py. Această actualizare reflectă compatibilitatea cu Odoo 19.0.

--- deltatech_category_group_equipment/__manifest__.py
+++ deltatech_category_group_equipment/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Deltatech Equipment Category Group",
     "summary": "Groups for equipments, related to internal categories groups",
-    "version": "18.0.0.0.2",
+    "version": "19.0.0.0.2",
     "author": "Terrabit, Dan Stoica",
     "website": "https://www.terrabit.ro",
     "category": "Other",
